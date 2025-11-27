17b0b7bbe6100bcf6a1efa1adfb65499a812c3af
s1140326-sudo <s1140326@dmhs.kh.edu.tw>
2025-11-27 09:45:45 +0800

    HW7
    HW7

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    int a =8;
-    int b =3;
-    int c =a-b;
+int main() {
+    double height, weight, bmi;
+
+    cout << "請輸入身高(cm): ";
+    cin >> height;
+
+    cout << "請輸入體重(kg): ";
+    cin >> weight;
+
+    // 將身高轉換為公尺
+    height = height / 100.0;
+
+    // 計算 BMI
+    bmi = weight / (height * height);
+
+    cout << "您的 BMI = " << bmi << endl;
+
     
-    std::cout <<(a>b) << "/t";
-    std::cout <<(a<c) << "/t";
-    std::cout <<(a!=b) << "/t";
-    std::cout <<(a== b) << "/t";
-    std::cout <<(a>=b) << "/t";
-
-
+    if (bmi <18.5) {
+        cout << "你可能過輕" << endl;
+    } else if(bmi<24.9)  {
+        cout << "你屬於正常" << endl;       
+    }else{
+        cout <<"你可能過重";
+    }
     return 0;
 }
